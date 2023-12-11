2d28866915884ce8eade7c5a7976c465af920f47
savvyh <7277@holbertonstudents.com>
2023-12-11 14:57:40 +0000

    Add the file shell.h that receive all prototypes & library that we have to use

---
+++ shell.h
@@ -0,0 +1,13 @@
+#ifndef __SHELL__
+#define __SHELL__
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv);
+void parse_user_input(char *input);
+
+#endif
