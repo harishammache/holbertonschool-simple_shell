689aa49731cc561510ed9ca9db1e6dc593579a90
savvyh <7277@holbertonstudents.com>
2023-12-12 10:13:28 +0000

    Fix the shell.h by adding prototypes functions & adding a struct path_list

--- shell.h
+++ shell.h
@@ -6,8 +6,23 @@
 #include <string.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <fcntl.h>
+
+/**
+ * struct node - node of the path
+ *
+ * @directory: contains a directory
+ * Each directory is a pointer to the first node of a linked list
+ * @next: pointer to the next node
+*/
+typedef struct node
+{
+	char *directory;
+	struct node *next;
+} path_list;
 
 int main(int argc, char **argv);
-void parse_user_input(char *input);
+void *parse_user_input(char *input);
+path_list *create_path_list (char *path);
 
 #endif
