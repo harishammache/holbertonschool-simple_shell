e7a5570cce40a85d96a5f4b61e0281cf8456bf87
savvyh <7277@holbertonstudents.com>
2023-12-13 14:47:34 +0000

    fix by copy the main that haris did

--- shell.c
+++ shell.c
@@ -1,44 +1,67 @@
-#include "SHELL.H"
+#include "shell.h"
 /**
  * main - UNIX command line interpreter
  * @argv: Number of arguments passed to the program
  * @argc: Array of strings representing the arguments
  *
- * Return: 0 if success
+ * Return: 0 if success , 1 if error
 */
-int main(__attribute__((unused))int argc, __attribute__((unused))char **argv)
+int main(void)
 {
-	int process_id;
+	pid_t process_id;
 	char *input = NULL;
 	size_t length = 0;
 	ssize_t read_byte;
+	char **env = environ;
+	char **args;
 
 	while (1)
 	{
-		/*fonction à part pour gérer interactif*/
-		if (isatty(STDIN_FILENO)) /*verifie si shell fonctionne en mode interactif ou non , Affiche un prompt*/
+		if (isatty(STDIN_FILENO))
 		{
 			printf("hsh $ ");
-			read_byte = getline(&input, &length, stdin);
-			if(read_byte == -1)
+		}
+		read_byte = getline(&input, &length, stdin);
+		if(read_byte == -1)
+		{
+			printf("\n");
+			break;
+		}
+		input[strcspn(input, "\n")] = 0;
+
+		if (strcmp(input, "exit\n") == 0)
+			break;
+		if (strcmp(input, "env\n") == 0)
+		{
+			for(; *env != NULL; env++)
 			{
-				printf("\n");
-				break;
+				char *env_1 = *env;
+				printf("%s\n", env_1);
 			}
 		}
 
+		args = parse_user_input(input);
+		if (args == NULL)
+			continue;
+
 		process_id = fork();
+		/*in child process*/
 		if (process_id == 0)
-        {
-            exit(EXIT_FAILURE);
-        }
+		{
+
+			execute_command(input);
+				printf("%d\n", process_id);
+		}
+		/*in parent process*/
 		else if (process_id > 0)
 		{
-			printf("%d\n", process_id);
+			wait(NULL);
 		}
+		/* if fork failes*/
 		else
 		{
 			perror("error");
+			return (1);
 		}
 	}
 	return (0);
