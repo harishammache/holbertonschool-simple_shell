ae400c3659eec238b303e0d6436bbdd12e74b874
savvyh <7277@holbertonstudents.com>
2023-12-19 09:28:34 +0000

    fix by adding a condition outside the while loop, on the function search_executable_files

--- search_executable_files.c
+++ search_executable_files.c
@@ -14,15 +14,14 @@ char *search_executable_files(path_list *head, const char *command)
 	char *pathname;
 	char *result = NULL;
 
+	if (head == NULL || head->directory == NULL)
+	{
+		fprintf(stderr, "Invalid path\n");
+		return (NULL);
+	}
+
 	while (head != NULL)
 	{
-		if (head == NULL || head->directory == NULL)
-		{
-			fprintf(stderr, "Directory path is NULL\n");
-			head = head->next;
-			continue;
-		}
-
 		pathname = malloc(strlen(head->directory) + strlen(command) + 2);
 		if (pathname == NULL)
 		{
